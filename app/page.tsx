9b29844dd07309ef6972504aba6e50313de647e4
johnroma <login@john.ro>
2025-09-20 21:34:14 +0200

    chore:focrce dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+export const runtime = 'nodejs';
+
 import { unstable_noStore as noStore } from 'next/cache';
 import Image from 'next/image';
 import Link from 'next/link';
